e80c82b82d6691f0c413be0f2b138d319ff3a1cc
AhmedElgindy <your_github_email>
2023-02-25 18:46:04 +0200

    remove the main page

--- lib/modules/home/home_screen.dart
+++ lib/modules/home/home_screen.dart
@@ -1,13 +1,6 @@
 // ignore_for_file: must_be_immutable, avoid_print
 
 import 'package:flutter/material.dart';
-import 'package:graduation/modules/auth/login/login_screen.dart';
-import 'package:graduation/modules/auth/register/register_screen.dart';
-
-import '../../shared/component/components.dart';
-import '../../shared/component/constants.dart';
-import '../../shared/component/helperfunctions.dart';
-import '../../shared/component/layout.dart';
 
 class HomeScreen extends StatelessWidget {
   var formKey = GlobalKey<FormState>();
@@ -15,40 +8,8 @@ class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return Stack(children: <Widget>[
-      Container(
-        color: Colors.red,
-        child: Image.asset(
-          'assets/images/3ef2582ea94414801edc3f61e80ee4c5.gif',
-          height: double.infinity,
-          width: double.infinity,
-          fit: BoxFit.fill,
-        ),
-      ),
-      Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            defaultButton(
-                text: "Log in",
-                onPressed: () {
-                  nextScreen(context, const LoginHome());
-                }),
-            SizedBox(
-              height: AppLayout.getHeigth(space2),
-            ),
-            defaultButton(
-                text: "Register",
-                onPressed: () {
-                  nextScreen(context, const Register());
-                }),
-            SizedBox(
-              height: AppLayout.getHeigth(space2),
-            ),
-            defaultButton(text: "Guest", onPressed: () {}),
-          ],
-        ),
-      )
-    ]);
+    return const Center(
+      child: Text("Home Page"),
+    );
   }
 }
